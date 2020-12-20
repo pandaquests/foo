41597a2ca9bede179c40583a579ed33b9426643d
pandaquests <hi.pandaquests@gmail.com>
2020-12-20 12:44:17 +0100

    Updates

---
+++ precache-manifest.ab94754997339ae842df26ce05334e29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff51cbcdb973d2d65b296ff1eba3a954",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa98749d712497f35cc2",
+    "url": "./static/css/main.b45986c1.chunk.css"
+  },
+  {
+    "revision": "1aa63ee61a46c9f56b1b",
+    "url": "./static/js/2.1095fe34.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1095fe34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa98749d712497f35cc2",
+    "url": "./static/js/main.cf44733b.chunk.js"
+  },
+  {
+    "revision": "84d5257c9a3829018c6f",
+    "url": "./static/js/runtime-main.024d61bb.js"
+  }
+]);
