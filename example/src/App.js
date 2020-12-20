2785d94f6d3b81d6dc2de14bb4993bc4ece71410
pandaquests <hi.pandaquests@gmail.com>
2020-12-20 12:43:49 +0100

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from '@pandaquests/foo'
 import '@pandaquests/foo/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent alText="image" imageSrc="http://picpig.com/5fb7h6jwof.jpg" blurSize="40" shadowTop="15" shadowLeft="0"/>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = ({ altText, imageSrc, blurSize, shadowTop, shadowLeft }) => {
+
+  return (
+    <div className={styles.imgContainer}>
+      <div style={{position: 'absolute', backgroundSize: 'cover', filter: 'blur(' + blurSize +'px)', zIndex: '-1', top: shadowTop + 'px', left: shadowLeft + 'px', width: '100%', height: '100%', backgroundImage: 'url(' + imageSrc + ')'}} />
+      <img src={imageSrc} alt={altText} />
+    </div>
+  )
 }
